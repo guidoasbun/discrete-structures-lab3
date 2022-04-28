eeb29cb441205b10613813d97f6064a60cf99a0d
GuidoAsbun <guido@asbun.io>
2022-04-27 17:17:22 -0700

    earthTemp()

--- functions.cpp
+++ functions.cpp
@@ -7,7 +7,15 @@
 
 int earthTemp(int temp, int numYears)
 {
-    return 0;
+    // Earth's current average temperature is 61 degerees
+// Fahrenheit. Suppose Earth's temperature rises by 3 degrees every
+// year.  Write a function that takes the starting temperature, a
+// number of years, and returns the temperature of the Earth after that
+// many years have passed.
+//
+// Do not use any loops or recursion.
+
+    return (temp + (numYears * 3));
 }
 
 int requiredFood(int startWeight, int foodNeeded, int finalWeight)
